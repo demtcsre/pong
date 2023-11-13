af0bf1f08338346d5cf86f306951095705241482
noxca <ahmadrizkidaffaa@gmail.com>
2023-11-13 17:19:35 +0700

    Paddle can move, but not smooth

--- pong.py
+++ pong.py
@@ -3,8 +3,9 @@ import pygame
 pygame.init()
 
 width, height = 800,600
-fps = 120
+fps = 60
 bg_color = (0,0,139)
+
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Pong Game")
 
@@ -12,24 +13,21 @@ pad_w, pad_h = 20, 200
 
 class Paddle():
     color = (102,102,255)
-    vel = 25
+    vel = 5
 
     def __init__(self,x,y,width,height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.static = True
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x,self.y,self.width,self.height))
 
-    def moveUp(self):
-        if not self.static and self.y > 0:
+    def move(self, up = True):
+        if up:
             self.y -= self.vel
-            
-    def moveDown(self):
-        if not self.static and self.y < height - self.height:
+        else:
             self.y += self.vel
 
 class Ball():
@@ -38,8 +36,10 @@ class Ball():
 
 def draw(screen, paddles):
     screen.fill(bg_color)
+    
     for paddle in paddles:
         paddle.draw(screen)
+
     pygame.display.update()
 
 def main():
@@ -51,11 +51,10 @@ def main():
     padds = [l_padd, r_padd]
 
     while run:
-
-        pygame.display.flip()
-        clock.tick(fps)
-
+        screen.fill(bg_color)
         draw(screen, padds)
+
+        clock.tick(fps)
 
         for event in pygame.event.get():
             if event.type == pygame.quit:
@@ -65,28 +64,27 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
-                if event.key == pygame.K_w:
+                if event.key == pygame.K_w and l_padd.y > 0:
+                    l_padd.move(up=True)
+                if event.key == pygame.K_s and l_padd.y <= height - l_padd.height:
                     l_padd.static = False
-                    l_padd.moveUp()
-                if event.key == pygame.K_s:
-                    l_padd.static = False
-                    l_padd.moveDown()
-                if event.key == pygame.K_UP:
+                    l_padd.move(up=False)
+                if event.key == pygame.K_UP and r_padd.y > 0:
                     r_padd.static = False
-                    r_padd.moveUp()
-                if event.key == pygame.K_DOWN:
+                    r_padd.move(up=True)
+                if event.key == pygame.K_DOWN and r_padd.y <= height - r_padd.height:
                     r_padd.static = False
-                    r_padd.moveDown()
+                    r_padd.move(up=False)
                     
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_w:
-                    l_padd.static = True
-                if event.key == pygame.K_s:
-                    l_padd.static = True
-                if event.key == pygame.K_UP:
-                    r_padd.static = True
-                if event.key == pygame.K_DOWN:
-                    r_padd.static = True
+            # elif event.type == pygame.KEYUP:
+            #     if event.key == pygame.K_w:
+            #         l_padd.static = True
+            #     if event.key == pygame.K_s:
+            #         l_padd.static = True
+            #     if event.key == pygame.K_UP:
+            #         r_padd.static = True
+            #     if event.key == pygame.K_DOWN:
+            #         r_padd.static = True
 
 
 if __name__ == "__main__":
